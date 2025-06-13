3b7619d4fa66c969233e8c6145dff871a483f334
plusdojo2025-michihsita <michishita-yuki-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 17:21:19 +0900

    Update ListServlet.java

--- source/src/main/java/servlet/ListServlet.java
+++ source/src/main/java/servlet/ListServlet.java
@@ -1,15 +1,21 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.AllListDao;
+import dto.AllListDto;
 
 /**
  * Servlet implementation class ListServlet
  */
-@WebServlet("/ListServlet")
+@WebServlet("/Mindshift-list")
 public class ListServlet extends CustomTemplateServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -25,6 +31,28 @@ public class ListServlet extends CustomTemplateServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		AllListDao AllListDao = new AllListDao();
+		AllListDto AllListDto = new AllListDto();
+		
+		//AIのカラム以外を記載
+		AllListDto.setId(Integer.parseInt(request.getParameter("id")));
+		AllListDto.setEmoStampId(Integer.parseInt(request.getParameter("emo_stamp_id")));
+		AllListDto.setAction("action");
+		AllListDto.setEmotionId(Integer.parseInt(request.getParameter("emotion_id")));
+		AllListDto.setFeedbacksId(Integer.parseInt(request.getParameter("feedbacks_id")));
+		String createdAtStr = request.getParameter("created_at"); // フォーム名に合わせる
+		Timestamp createdAt = Timestamp.valueOf(createdAtStr);    // 文字列 → Timestamp
+		AllListDto.setCreated_at(createdAt);  
+		AllListDto.setPlant("plant");
+		
+		 
+		
+		//userDto.setUserId(23);
+		
+		AllListDao.insert(AllListDto);
+		AllListDao.select(AllListDto);
+		AllListDao.update(AllListDto);
+		AllListDao.delete(AllListDto);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
