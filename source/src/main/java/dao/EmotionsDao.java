738b7e8d3484d0151151152ff94c4b6151ed532e
plusdojo2025-Watanabe <watanabe-momoko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 16:03:53 +0900

    a

--- source/src/main/java/dao/EmotionsDao.java
+++ source/src/main/java/dao/EmotionsDao.java
@@ -50,21 +50,119 @@ public class EmotionsDao extends CustomTemplateDao<EmotionsDto> {
 	}
 
 	@Override
-	public boolean insert(EmotionsDto dto) {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
+	public boolean insert(EmotionsDto dto)  {
+		{
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				// データベースに接続する
+				conn = conn();
+
+				// SQL文を準備する
+				String sql = """
+						insert emotions(action, emotion )
+								value(?, ?)
+						""";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setString(1, dto.getAction());
+				pStmt.setInt(2, dto.getEmotion());
+
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					ResultSet res = pStmt.getGeneratedKeys();
+					res.next();
+					dto.setEmoStamp(res.getInt(1));
+					result = true;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				// データベースを切断
+				close(conn);
+				}
+			
+			// 結果を返す
+			return result;
+		}
 	}
 
 	@Override
 	public boolean update(EmotionsDto dto) {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
+		{
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				// データベースに接続する
+				conn = conn();
+
+				// SQL文を準備する
+				String sql = """
+						update emotions set
+emo_stamp = ?
+,action = ?
+,emotion = ?
+						where 主キー　=?
+						""";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				pStmt.setInt(1, dto.getEmoStamp());
+				pStmt.setString(2, dto.getAction());
+				pStmt.setInt(3, dto.getEmotion());
+
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				// データベースを切断
+				close(conn);
+				}
+			
+			// 結果を返す
+			return result;
+		}
 	}
 
 	@Override
 	public boolean delete(EmotionsDto dto) {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
-	}
+		Connection conn = null;
+		boolean result = false;
 
+		try {
+			// JDBCドライバを読み込む
+			// データベースに接続する
+			conn = conn();
+
+			// SQL文を準備する
+			String sql = "DELETE FROM emotions";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			pStmt.setInt(1, dto.getEmoStamp());
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			close(conn);
+			}
+		
+		// 結果を返す
+		return result;
+	}
 }
