fd86b7ec87986ee6b92ad5997e11a92b1f889dd8
plusdojo2025-Watanabe <watanabe-momoko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 15:06:22 +0900

    Update PositiveServlet.java

--- source/src/main/java/servlet/PositiveServlet.java
+++ source/src/main/java/servlet/PositiveServlet.java
@@ -1,15 +1,15 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.EmotionsDao;
-import dao.UsersDao;
-import dto.EmotionsDto;
-import dto.UsersDto;
+//import dao.EmotionsDao;
+//import dto.EmotionsDto;
 
 /**
  * Servlet implementation class PositiveServlet
@@ -29,28 +29,51 @@ public class PositiveServlet extends CustomTemplateServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		EmotionsDao emotionDao = new EmotionsDao();
-		EmotionsDto emotionDto = new EmotionsDto();
-		
-		//AIのカラム以外を記載
-		emotionDto.setAction("setAction");
-		emotionDto.setEmotion(0);
+//		EmotionsDao emotionDao = new EmotionsDao();
+//		EmotionsDto emotionDto = new EmotionsDto();
+//		
+//		//AIのカラム以外を記載
+//		emotionDto.setAction("setAction");
+//		emotionDto.setEmotion(0);
+//				
+//		//userDto.setUserId(23);
+//				
+//		emotionDao.insert(emotionDto);
+//		emotionDao.select(emotionDto);
+//		emotionDao.update(emotionDto);
+//		emotionDao.delete(emotionDto);
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		
+		//ログインしていなかった場合、ログイン画面にリダイレクト処理をする。(HomeServletをそのままコピーしてもらって大丈夫です。)
+				if(checkNoneLogin(request, response)) {
+					return;
+				}
+				// ホームページにフォワードする
+			    RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/positive.jsp");
 				
-		//userDto.setUserId(23);
-				
-		emotionDao.insert(emotionDto);
-		emotionDao.select(emotionDto);
-		emotionDao.update(emotionDto);
-		emotionDao.delete(emotionDto);
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+				dispatcher.forward(request, response);
+			}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+			@Override
+			//「checkNoneLogin」、「logout」の処理を加えてください。(logoutはナビゲーションがあるページのみに適用する。)
+			protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+				// TODO 自動生成されたメソッド・スタブ
+				
+				//ログインしていなかった場合、ログイン画面にリダイレクト処理をする。(HomeServletをそのままコピーしてもらって大丈夫です。)
+				if(checkNoneLogin(request, response)) {
+					return;
+				}	
+				//ログアウト処理(HomeServletをそのままコピーしてもらって大丈夫です。)
+				if (logout(request, response)) {
+					return;
+				}
+				
+				if (account_del(request, response)) {
+					return;
+				}
+				
+				
+			}
+			
 
 }
