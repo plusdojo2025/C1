60405660c602dd7f6dad5f1cf5991fe22c880c39
plusdojo2025-Watanabe <watanabe-momoko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 14:39:22 +0900

    stamps dao,dto 修正

--- source/src/main/java/dto/stampsDto.java
+++ source/src/main/java/dto/stampsDto.java
@@ -4,19 +4,28 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class stampsDto extends CustomTemplateDto implements Serializable{
+	private int userId;
 	private int weekStamps;
 	private Date createdAt;
 
-	public stampsDto(int weekStamps, Date createdAt) {
+	public stampsDto(int userId, int weekStamps, Date createdAt) {
 		super();
+		this.userId = userId;
 		this.weekStamps = weekStamps;
 		this.createdAt = createdAt;
 	}
 	
 	public stampsDto() {
-		this(0, new Date());
+		this(0, 0, new Date());
 	}
 
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 	public int getWeekStamps() {
 		return weekStamps;
 	}
